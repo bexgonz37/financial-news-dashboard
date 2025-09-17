08beb253a4aa44d5b0c89bd575a2cbc9f95ce558
bexgonz37 <bexgonz37@gmail.com>
2025-09-17 15:45:49 -0600

    fix: add not-found page and whoami endpoint for 404 debugging

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Hiar++zjjYXPJaNsD3C8S3R82imcMwuJROM0X3VZNlw=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"YNCzDWOK20U9xcpeT4E6cVAe4mX43NspYZr2Dn4GCRU=\"}"
